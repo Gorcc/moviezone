358811124626d3d6856354ae562c840fab55c6c7
Deniz Görkem Ater <gorkem.ater1@hotmail.com>
2023-03-28 02:30:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MovieCard from "./MovieCard";
+import popularSearches from "./popularSearches";
+import "./App.css";
+import SearchIcon from "./search.svg";
+// 607906a4
+const API_URL = "http://www.omdbapi.com?apikey=607906a4";
+const random = Math.floor(Math.random() * popularSearches.length);
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = React.useState([]);
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  React.useEffect(() => {
+    searchMovies(popularSearches[random]);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Moviezone</h1>
+      <div className="search">
+        <input
+          placeholder="Search any movie"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="Search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+      <h2>
+        Search results for {searchTerm ? searchTerm : popularSearches[random]}
+      </h2>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/popularSearches.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+const popularSearches = ["Batman","Superman","Avengers","John Wick","Spider-Man"]
+
+
+
+
+export default popularSearches;
